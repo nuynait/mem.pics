6e523c7b51e5ca106d4b3601bcfbf79eaaadcc01
Jerry Shan <shan199105@gmail.com>
2014-06-09 00:04:25 -0400

    init Xcode Swift Project
    - init xcode  Swift Project
    - add MainVC Class

---
+++ ios/MemPics/MemPics/MainVC.swift
@@ -0,0 +1,40 @@
+//
+//  MainVC.swift
+//  MemPics
+//
+//  Created by Jerry Shan on 2014-06-08.
+//  Copyright (c) 2014 Jerry Shan. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        // Custom initialization
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // #pragma mark - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue?, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
