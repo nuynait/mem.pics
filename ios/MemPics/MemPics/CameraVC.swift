95efad372d1c0178896fa7f8b90f60cb5e576472
Jerry Shan <shan199105@gmail.com>
2014-06-11 00:24:44 -0400

    Full Functional Camera, Image Save to Camera Roll
    - add a dismissViewButton on camera View Controller
    - add a render button function to avoid code duplicate
    - add a camera delegate
    - camera delegate: cancel method and finish method
    - in cancel method, dismiss the cameraUI
    - in finish method, first check if it is a still image, if it is a
      still image, save the image to the camera roll.

--- ios/MemPics/MemPics/CameraVC.swift
+++ ios/MemPics/MemPics/CameraVC.swift
@@ -8,10 +8,14 @@
 
 import UIKit
 
-class CameraVC: UIViewController {
+class CameraVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     // Button to Call Camera View
     var cameraViewButton:UIButton = UIButton.buttonWithType(UIButtonType.System) as UIButton;
+    var lastViewButton:UIButton = UIButton.buttonWithType(UIButtonType.System) as UIButton;
+    
+    // Image Picker View Controller
+    var cameraUI:UIImagePickerController = UIImagePickerController();
     
     
     init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
@@ -24,21 +28,36 @@ class CameraVC: UIViewController {
         self.view = mainView;
         
         self.view.addSubview(self.cameraViewButton);
+        self.view.addSubview(self.lastViewButton);
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Setup the Subviews
-        self.cameraViewButton.setTitle("Launch Camera", forState: UIControlState.Normal);
-        self.cameraViewButton.sizeToFit();
-        self.cameraViewButton.frame = CGRectMake((self.view.frame.width - self.cameraViewButton.frame.width) / 2,
-            100,
-            self.cameraViewButton.frame.width,
-            self.cameraViewButton.frame.height);
+        buttonLoad(self.cameraViewButton, title: "Launch Camera", state: UIControlState.Normal, yPoint:100);
         self.cameraViewButton.addTarget(self, action: "cameraViewButtonPressed:", forControlEvents: UIControlEvents.TouchUpInside);
+        
+        buttonLoad(self.lastViewButton, title: "Last View", state: UIControlState.Normal, yPoint: self.cameraViewButton.frame.maxY + 10);
+        self.lastViewButton.addTarget(self, action: "lastViewButtonPressed:", forControlEvents: UIControlEvents.TouchUpInside);
     }
     
+    // render a button in the middle of the line for x-axis
+    // input variable: 
+    // 1. button title and state. 
+    // 2. y-axis point
+    func buttonLoad(target:UIButton, title:NSString, state:UIControlState, yPoint:CGFloat) {
+        target.setTitle(title, forState: state);
+        target.sizeToFit();
+        target.frame = CGRectMake((self.view.frame.width - target.frame.width) / 2,
+            yPoint,
+            target.frame.width,
+            target.frame.height);
+    }
+    
+    func lastViewButtonPressed(sender:UIButton) {
+        self.dismissModalViewControllerAnimated(true);
+    }
     
     // Launch Camera Picker Button Pressed
     // Use LaunchCamera function to launch camera picker
@@ -55,16 +74,56 @@ class CameraVC: UIViewController {
     // Then present the camera view
     func launchCamera() -> Bool {
         if (UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)){
-            var cameraUI:UIImagePickerController = UIImagePickerController();
-            cameraUI.sourceType = UIImagePickerControllerSourceType.Camera;
-            cameraUI.allowsEditing = false;
-            self.presentModalViewController(cameraUI, animated: true);
+            self.cameraUI.sourceType = UIImagePickerControllerSourceType.Camera;
+            self.cameraUI.delegate = self;
+            self.cameraUI.allowsEditing = false;
+            self.presentModalViewController(self.cameraUI, animated: true);
             return true;
         }
         return false
     }
     
+    // *********************************************************************************************
+    // Camera Delegate Method
     
+    // For responding to the user tapping cancel
+    func imagePickerControllerDidCancel (picker:UIImagePickerController) {
+        // dismiss the camera picker when user tap cancel
+        self.dismissModalViewControllerAnimated(true);
+    }
+    
+    // For responding to the user accepting a newly-captured picture
+    func imagePickerController(_picker: UIImagePickerController!, didFinishPickingMediaWithInfo info: NSDictionary!) {
+        var mediaType:NSString = info.objectForKey(UIImagePickerControllerMediaType) as NSString;
+        var originalImage:UIImage?;
+        var editedImage:UIImage?;
+        var imageToSave:UIImage?;
+        
+        
+        // Handle a still image capture
+        if (mediaType.isEqualToString("public.image")){
+            editedImage = info.objectForKey(UIImagePickerControllerEditedImage) as? UIImage;
+            originalImage = info.objectForKey(UIImagePickerControllerOriginalImage) as? UIImage;
+            
+            if (editedImage) {
+                imageToSave = editedImage;
+            }
+            else {
+                imageToSave = originalImage;
+            }
+            
+            // Save image to the Camera Roll
+            UIImageWriteToSavedPhotosAlbum(imageToSave, nil, nil, nil);
+        }
+        else {
+            println("ERROR: If Case Passed, test FAILED, mediaType = \(mediaType)");
+        }
+        
+        // Dismiss image Picker
+        self.dismissModalViewControllerAnimated(true);
+        
+    }
+    // *********************************************************************************************
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
